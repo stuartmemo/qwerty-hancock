3ae3e3bf0b2221754d696c0d5e13c9a772b5fe3f
Stuart Memo <stuartmemo@gmail.com>
2012-07-15 23:56:47 +0100

    removed synth

--- demo/sine-synth.js
+++
@@ -1,9 +0,0 @@
-var context = new webkitAudioContext(),
-    sineWave = context.createOscillator(),
-    gainNode = context.createGainNode();
-
-sineWave.connect(gainNode);
-gainNode.connect(context.destination);
-
-sineWave.noteOn(0);
-gainNode.gain.value = 0;
