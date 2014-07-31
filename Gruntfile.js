0f6f59b0bdbd5e61b86e33f04086b7a461e9e0e6
Stuart Memo <stuartmemo@gmail.com>
2014-07-31 15:13:23 +0100

    grunt copy and travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,14 +5,13 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        uglify: {
-            all: {
-                files: {
-                    'qwerty-hancock.min.js': 'qwerty-hancock.js'
-                },
-                options: {
-                    banner: '/* <%= pkg.title %> <%= pkg.version %> (c) 2013-14 Stuart Memo */\n'
-                }
+        copy: {
+            main: { 
+                expand: true,
+                filter: 'isFile',
+                cwd: 'src/',
+                src: 'qwerty-hancock.js',
+                dest: 'dist/'
             }
         },
 
@@ -20,15 +19,28 @@ module.exports = function (grunt) {
             unit: {
                 configFile: 'karma-conf.js'
             }
-        }
+        },
+
+        uglify: {
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['src/<%= pkg.name %>.js']
+                }
+            },
+            options: {
+                banner: '/* <%= pkg.title %> <%= pkg.version %> (c) 2012-<%= grunt.template.today("yy") %> Stuart Memo */\n'
+            }
+        },
+
     });
 
     // Load plugins.
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-requirejs');
     grunt.loadNpmTasks('grunt-karma');
 
     // Tasks.
     grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('build', ['copy', 'uglify']);
     grunt.registerTask('test', ['karma']);
 };
