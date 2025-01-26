cb23997694bc3be166fc1f0ec62dea0d806bd2c9
Stuart Memo <stuartmemo@gmail.com>
2025-01-26 17:45:21 +0000

    Update dependencies and testing framework
    Update dependencies and testing framework, and remove Grunt.
    
    * **Dependencies**: Remove Grunt-related dependencies and add Jest, Playwright, and Uglify-js in `package.json`.
    * **Scripts**: Add npm scripts for `copy`, `uglify`, `test`, and `build` in `package.json`.
    * **Testing Framework**: Update `tests/qh-node-spec.js` and `tests/qh-tests.js` to use Jest instead of Jasmine.
    * **Remove Grunt**: Delete `Gruntfile.js` and `karma-conf.js`.

--- tests/qh-tests.js
+++ tests/qh-tests.js
@@ -1,30 +1,30 @@
-describe('Qwerty Hancock tests', function () {
+describe('Qwerty Hancock tests', () => {
     'use strict';
 
-    var element;
+    let element;
 
-    beforeEach(function () {
+    beforeEach(() => {
         element = document.createElement('div');
         element.id = 'keyboard';
         document.body.appendChild(element);
     });
 
-    it('Can create keyboard without any user settings', function () {
-        var qh = new QwertyHancock();
+    it('Can create keyboard without any user settings', () => {
+        const qh = new QwertyHancock();
 
         expect(element.id).toBe('keyboard');
         expect(element.querySelectorAll('li').length).toBeGreaterThan(0);
     });
 
-    it('Can create keyboard with user specified dimensions', function () {
-        var qh = new QwertyHancock({width: 500, height: 300});
+    it('Can create keyboard with user specified dimensions', () => {
+        const qh = new QwertyHancock({width: 500, height: 300});
 
         expect(element.offsetWidth).toBe(500);
         expect(element.offsetHeight).toBe(300);
     });
 
-    it('Keyboard without specified dimensions uses element dimensions', function () {
-        var qh;
+    it('Keyboard without specified dimensions uses element dimensions', () => {
+        let qh;
 
         element.style.width = '200px';
         element.style.height = '100px';
@@ -35,61 +35,61 @@ describe('Qwerty Hancock tests', function () {
         expect(element.querySelector('ul').style.height).toBe(element.style.height);
     });
 
-    it('White keys should be white by default', function () {
-        var qh = new QwertyHancock(),
-            white_keys = element.querySelectorAll('li[data-note-type="white"]');
+    it('White keys should be white by default', () => {
+        const qh = new QwertyHancock();
+        const white_keys = element.querySelectorAll('li[data-note-type="white"]');
 
-        for (var i = 0; i < white_keys.length; i++) {
-            expect(white_keys[i].style.backgroundColor).toBe('rgb(255, 255, 255)');
-        }
+        white_keys.forEach((key) => {
+            expect(key.style.backgroundColor).toBe('rgb(255, 255, 255)');
+        });
     });
 
-    it('Black keys should be black by default', function () {
-        var qh = new QwertyHancock(),
-            black_keys = element.querySelectorAll('li[data-note-type="black"]');
+    it('Black keys should be black by default', () => {
+        const qh = new QwertyHancock();
+        const black_keys = element.querySelectorAll('li[data-note-type="black"]');
 
-        for (var i = 0; i < black_keys.length; i++) {
-            expect(black_keys[i].style.backgroundColor).toBe('rgb(0, 0, 0)');
-        }
+        black_keys.forEach((key) => {
+            expect(key.style.backgroundColor).toBe('rgb(0, 0, 0)');
+        });
     });
 
-    it('White keys should be user defined colour', function () {
-        var qh = new QwertyHancock({whiteKeyColour: '#333'}),
-            white_keys = element.querySelectorAll('li[data-note-type="white"]');
+    it('White keys should be user defined colour', () => {
+        const qh = new QwertyHancock({whiteKeyColour: '#333'});
+        const white_keys = element.querySelectorAll('li[data-note-type="white"]');
 
-        for (var i = 0; i < white_keys.length; i++) {
-            expect(white_keys[i].style.backgroundColor).toBe('rgb(51, 51, 51)');
-        }
+        white_keys.forEach((key) => {
+            expect(key.style.backgroundColor).toBe('rgb(51, 51, 51)');
+        });
     });
 
-    it('Black keys should be user defined colour', function () {
-        var qh = new QwertyHancock({blackKeyColour: 'red'}),
-            black_keys = element.querySelectorAll('li[data-note-type="black"]');
+    it('Black keys should be user defined colour', () => {
+        const qh = new QwertyHancock({blackKeyColour: 'red'});
+        const black_keys = element.querySelectorAll('li[data-note-type="black"]');
 
-        for (var i = 0; i < black_keys.length; i++) {
-            expect(black_keys[i].style.backgroundColor).toBe('red');
-        }
+        black_keys.forEach((key) => {
+            expect(key.style.backgroundColor).toBe('red');
+        });
     });
 
-    it('First key on keyboard should be user defined note', function () {
-        var qh = new QwertyHancock({startNote: 'C4'}),
-            first_white_key = element.querySelector('li[data-note-type="white"]');
+    it('First key on keyboard should be user defined note', () => {
+        const qh = new QwertyHancock({startNote: 'C4'});
+        const first_white_key = element.querySelector('li[data-note-type="white"]');
 
         expect(first_white_key.id).toBe('C4');
     });
 
-    it('When user presses key on computer keyboard, related keyboard key should change colour', function () {
-        var qh = new QwertyHancock(),
-            c4_key = document.querySelector('#C4');
+    it('When user presses key on computer keyboard, related keyboard key should change colour', () => {
+        const qh = new QwertyHancock();
+        const c4_key = document.querySelector('#C4');
 
         pressKey(65);
 
         expect(c4_key.style.backgroundColor).toBe('yellow');
     });
 
-    it('When user presses modifier key on computer keyboard, related keyboard key should not change colour', function () {
-        var qh = new QwertyHancock(),
-            d4_key = document.querySelector('#D4');
+    it('When user presses modifier key on computer keyboard, related keyboard key should not change colour', () => {
+        const qh = new QwertyHancock();
+        const d4_key = document.querySelector('#D4');
 
         pressKey(83, { metaKey: true });
 
@@ -97,7 +97,7 @@ describe('Qwerty Hancock tests', function () {
         expect(d4_key.style.backgroundColor).toBe('rgb(255, 255, 255)');
     });
 
-    afterEach(function () {
+    afterEach(() => {
         document.body.removeChild(element);
     });
 });
